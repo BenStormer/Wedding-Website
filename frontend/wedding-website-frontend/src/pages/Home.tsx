c51c330f124cbfcb7b4b7c96e706a7d9befe0d98
Benjamin Stormer <benjaminstormer1@gmail.com>
2025-09-20 17:11:54 -0500

    Add countdown timer

--- frontend/wedding-website-frontend/src/pages/Home.tsx
+++ frontend/wedding-website-frontend/src/pages/Home.tsx
@@ -3,7 +3,7 @@ import './Home.css';
 const Home = () => {
   return (
     <div className="body">
-      <div className="engagement-photo"></div>
+      <LandingImage />
       <OverviewDetails />
       <div className="rsvp"></div>
       <div className="other-page-nudges"></div>
@@ -13,10 +13,28 @@ const Home = () => {
   );
 };
 
+const LandingImage = () => {
+  return (
+    <div className="engagement-photo-container">
+      <div className="engagement-photo"></div>
+      <div className="countdown">In {getDaysUntil()} days...</div>
+    </div>
+  );
+};
+
+function getDaysUntil(): number {
+  const weddingDate = new Date('2026-10-11');
+  const now = new Date();
+  const timeDifference = weddingDate.getTime() - now.getTime();
+  const daysUntil = Math.ceil(timeDifference / (1000 * 60 * 60 * 24));
+  return daysUntil;
+}
+
 const OverviewDetails = () => {
   return (
     <div className="overview-details">
       <h1>Celebrate the wedding of Aspen and Benjamin!</h1>
+      <div className="overview-detail-panels"></div>
     </div>
   );
 };
